e7f2a60bf893df88fdffbfe115d6f60b6019ea79
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-04-27 15:41:19 -0500

    initial

---
+++ src/test/java/com/jacob/val/ValApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.val;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
