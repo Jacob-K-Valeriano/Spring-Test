cf250f36b31cd54a374dcb28dcb70498eb133596
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-04-28 23:57:04 -0600

    began exercise, created Math/PostController class

---
+++ src/main/java/com/jacob/val/controller/MathController.java
@@ -0,0 +1,14 @@
+package com.jacob.val.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+@ResponseBody
+public class MathController {
+}
---
+++ src/main/java/com/jacob/val/controller/PostController.java
@@ -0,0 +1,4 @@
+package com.jacob.val.controller;
+
+public class PostController {
+}
